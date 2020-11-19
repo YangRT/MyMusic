e530c9c944640c92c240dd3ed56ac833046eb9ac
YangRT <1946464079@qq.com>
2020-11-20 00:55:33 +0800

    新增login包及loginViewModel

---
+++ app/src/main/java/com/example/mymusic/login/LoginViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.mymusic.login
+
+import androidx.lifecycle.ViewModel
+
+
+/**
+ * @program: MyMusic
+ *
+ * @description: 登录viewmodel
+ *
+ * @author: YangRT
+ *
+ * @create: 2020-11-20 00:54
+ **/
+
+class LoginViewModel: ViewModel(){
+}
