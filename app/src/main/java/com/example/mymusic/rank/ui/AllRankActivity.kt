41d0ce927860d5c64938b483efdea8a6ff1d779f
杨荣涛 <yangrongtao@bytedance.com>
2020-12-09 10:53:25 +0800

    排行榜UI准备工作

--- app/src/main/java/com/example/mymusic/rank/ui/AllRankActivity.kt
+++ app/src/main/java/com/example/mymusic/rank/ui/AllRankActivity.kt
@@ -1,12 +1,54 @@
 package com.example.mymusic.rank.ui
 
+import android.content.Intent
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.mymusic.R
 import com.example.mymusic.base.BaseActivity
+import com.example.mymusic.databinding.ActivityAllRankBinding
+import com.example.mymusic.search.ui.SearchActivity
 
 class AllRankActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityAllRankBinding
+    private lateinit var viewModel: AllRankViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_all_rank)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_all_rank)
+        binding.lifecycleOwner = this
+        viewModel =  ViewModelProvider(this).get(AllRankViewModel::class.java)
+        initView()
+    }
+
+    private fun initView() {
+        setSupportActionBar(binding.toolbarRankAll)
+        val upArrow = ContextCompat.getDrawable(this, R.drawable.abc_ic_ab_back_material)
+        upArrow?.setColorFilter(ContextCompat.getColor(this, R.color.colorMain), PorterDuff.Mode.SRC_ATOP)
+        supportActionBar?.setHomeAsUpIndicator(upArrow)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
+        binding.toolbarTitle.text = "播单"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.singer_list_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.singer_list_search -> {
+                val intent = Intent(this, SearchActivity::class.java)
+                startActivity(intent)
+            }
+            android.R.id.home -> { finish() }
+        }
+        return true
     }
 }
--- app/src/main/java/com/example/mymusic/rank/ui/AllRankAdapter.kt
+++ app/src/main/java/com/example/mymusic/rank/ui/AllRankAdapter.kt
@@ -1,4 +1,14 @@
 package com.example.mymusic.rank.ui
 
-class AllRankAdapter {
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseViewHolder
+import com.example.mymusic.R
+import com.example.mymusic.rank.model.RankInfo
+
+class AllRankAdapter(data: MutableList<RankInfo>) :
+    BaseQuickAdapter<RankInfo, BaseViewHolder>(R.layout.item_rank_info , data) {
+
+    override fun convert(holder: BaseViewHolder, item: RankInfo) {
+        TODO("Not yet implemented")
+    }
 }
