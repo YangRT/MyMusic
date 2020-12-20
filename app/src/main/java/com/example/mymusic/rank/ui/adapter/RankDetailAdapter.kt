4b08396cb55a354f5b322d82c77d8c8f5b5dd5a3
杨荣涛 <yangrongtao@bytedance.com>
2020-12-20 17:36:21 +0800

    UI调整

--- app/src/main/java/com/example/mymusic/rank/ui/adapter/RankDetailAdapter.kt
+++ app/src/main/java/com/example/mymusic/rank/ui/adapter/RankDetailAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.mymusic.rank.ui.adapter
 
+import android.graphics.Color
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.example.mymusic.R
@@ -10,6 +11,11 @@ class RankDetailAdapter(data: MutableList<Track>) :
 
     override fun convert(holder: BaseViewHolder, item: Track) {
         holder.setText(R.id.rank_detail_title, item.name)
+        if (holder.layoutPosition < 3) {
+            holder.setTextColor(R.id.rank_detail_no, Color.RED)
+        }else {
+            holder.setTextColor(R.id.rank_detail_no, Color.BLACK)
+        }
         holder.setText(R.id.rank_detail_no, "${holder.layoutPosition + 1}")
         if (item.ar.isNotEmpty()) {
             holder.setText(R.id.rank_detail_author, item.ar[0].name)
